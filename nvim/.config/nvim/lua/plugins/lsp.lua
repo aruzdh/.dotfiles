6b702a1f7ba27a0b21a7cb4b3a658f5d2c1211a4
aruzdh <aruzdh@gmail.com>
2025-07-06 13:58:00 -0600

    chore: Remove Unnecessary {}

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,25 +1,23 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        clangd = {
-          cmd = {
-            "clangd",
-            "--background-index",
-            "--clang-tidy",
-          },
-          init_options = {
-            fallbackFlags = { "--std=c23" },
-          },
-          filetypes = { "c", "cpp", "objc", "objcpp" },
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      clangd = {
+        cmd = {
+          "clangd",
+          "--background-index",
+          "--clang-tidy",
         },
+        init_options = {
+          fallbackFlags = { "--std=c23" },
+        },
+        filetypes = { "c", "cpp", "objc", "objcpp" },
       },
-      diagnostics = {
-        update_in_insert = true,
-        virtual_text = {
-          prefix = "🐛",
-        },
+    },
+    diagnostics = {
+      update_in_insert = true,
+      virtual_text = {
+        prefix = "🐛",
       },
     },
   },
