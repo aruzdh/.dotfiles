5985e27e9c18da36b5f2411ca2d4cb20eb485992
aruzdh <aruzdh@gmail.com>
2025-07-06 19:57:14 -0600

    feat: Add keymap to pin the current file in Typst

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -13,6 +13,25 @@ return {
         },
         filetypes = { "c", "cpp", "objc", "objcpp" },
       },
+      tinymist = {
+        on_attach = function(client, bufnr)
+          vim.keymap.set("n", "<leader>tp", function()
+            client:exec_cmd({
+              title = "pin",
+              command = "tinymist.pinMain",
+              arguments = { vim.api.nvim_buf_get_name(0) },
+            }, { bufnr = bufnr })
+          end, { desc = "Tinymist Pin", noremap = true })
+
+          vim.keymap.set("n", "<leader>tu", function()
+            client:exec_cmd({
+              title = "unpin",
+              command = "tinymist.pinMain",
+              arguments = { vim.v.null },
+            }, { bufnr = bufnr })
+          end, { desc = "Tinymist Unpin", noremap = true })
+        end,
+      },
     },
     diagnostics = {
       update_in_insert = true,
