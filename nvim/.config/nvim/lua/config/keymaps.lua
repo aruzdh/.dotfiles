f79a24cfe1544588ffcd987c4f0a239d28a327b5
aruzdh <aruzdh@gmail.com>
2025-07-06 19:56:29 -0600

    chore: Change keymap of Undo Tree

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,11 +1,11 @@
 local map = vim.keymap.set
 
 -- UNDOTREETOGGLE
-map("n", "<leader>t", function()
+map("n", "<leader>cu", function()
   vim.cmd.UndotreeToggle()
 end, { desc = "UndoTreeToggle" })
 
 -- TYPST PREVIEW
-map("n", "<leader>p", function()
+map("n", "<leader>tv", function()
   vim.cmd.TypstPreview()
 end, { desc = "Typst Preview" })
