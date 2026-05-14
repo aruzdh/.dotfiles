dd69aaad70b0ff77076ee1f372bcb0b461ad5f53
Aru <aruzdh@gmail.com>
2026-05-14 09:36:14 -0600

    feat: add logic to handle dark and ligth Rocq theme

--- nvim/.config/nvim/lua/aru/core/autocmds.lua
+++ nvim/.config/nvim/lua/aru/core/autocmds.lua
@@ -2,10 +2,12 @@ local function apply_custom_highlights()
   local hl = vim.api.nvim_set_hl
   local is_light = vim.o.background == "light"
   local cc_bg = is_light and "#286983" or "#ea9d34"
+  local rocq_checked = is_light and "#CCE6EB" or "#266983"
+  local comment_color = is_light and "#666666" or "#eeeeee"
 
   local bold_style = { bold = true }
   local dim_style = { fg = "#666666" }
-  local comment_style = { fg = "#666666", italic = true }
+  local comment_style = { fg = comment_color, italic = true }
 
   local highlights = {
     Comment = comment_style,
@@ -23,7 +25,7 @@ local function apply_custom_highlights()
     LspReferenceText = { fg = "#000000", bg = "#A2BE20", bold = true },
     LspReferenceRead = { link = "LspReferenceText" },
     LspReferenceWrite = { link = "LspReferenceText", underline = true },
-    CoqtailChecked = { bg = "#EBF990" },
+    CoqtailChecked = { bg = rocq_checked },
 
     ColorColumn = { bg = cc_bg },
   }
