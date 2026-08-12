db6c51e9a88be967c8ed33534f57377fb6169658
Aru <aruzdh@gmail.com>
2026-08-12 14:40:55 -0600

    feat(pnpm): update PATH

--- zsh/.zsh/exports.sh
+++ zsh/.zsh/exports.sh
@@ -20,10 +20,10 @@ export GPG_TTY=$TTY
 
 # --------------------------- PNPM ---------------------------
 
-export PNPM_HOME="$HOME/Library/pnpm"
+export PNPM_HOME="/Users/aru/Library/pnpm"
 case ":$PATH:" in
-*":$PNPM_HOME:"*) ;;
-*) export PATH="$PNPM_HOME:$PATH" ;;
+*":$PNPM_HOME/bin:"*) ;;
+*) export PATH="$PNPM_HOME/bin:$PATH" ;;
 esac
 
 # --------------------------- JAVA ---------------------------
